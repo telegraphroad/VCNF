15cb11159a4c0cb744619004c8f2b852e2e52e69
telegraphroad <saba.amiri@outlook.com>
2022-04-22 12:50:39 +0200

    bug fix

--- normflow/distributions/base.py
+++ normflow/distributions/base.py
@@ -155,7 +155,8 @@ class MixtureofMultivariateGaussians(BaseDistribution):
 
     def forward(self, num_samples=1):
         print('~~~1',self.loc.device,self.scale.device,self.w.device)
-        z = self.gmm.sample([num_samples])
+        ns = torch.tensor([num_samples],device='cuda')
+        z = self.gmm.sample(ns)
         print(z)
         #print('~~~2',self.loc.is_leaf,self.scale.is_leaf,self.w.is_leaf)
         log_prob= self.gmm.log_prob(z)
