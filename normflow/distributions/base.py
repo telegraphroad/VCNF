f8b2116a84543aa0ed6a260462e65337e2aeb921
telegraphroad <saba.amiri@outlook.com>
2022-04-22 16:08:40 +0200

    bug fix

--- normflow/distributions/base.py
+++ normflow/distributions/base.py
@@ -59,7 +59,7 @@ class MultivariateGaussian(BaseDistribution):
                 self.register_buffer("scale", torch.eye(self.n_dim,dtype=torch.double,device='cuda'))
 
         self.mvn = D.MultivariateNormal(self.loc, self.scale)
-        print('~~~1',self.gmm.mixture_distribution.probs)
+        
     def forward(self, num_samples=1):
         #print('~~~1',self.gmm.mixture_distribution.probs)
         
