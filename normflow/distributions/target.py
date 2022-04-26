05f5162e0f988d1e2be5ba4cbbac6e16b8d9e00d
telegraphroad <saba.amiri@outlook.com>
2022-04-26 11:48:02 +0200

    accordeon

--- normflow/distributions/target.py
+++ normflow/distributions/target.py
@@ -65,6 +65,7 @@ class NealsFunnel(Target):
     def __init__(self):
         super().__init__()
         self.n_dims = 2
+        self.max_log_prob = 0.
 
     # def sample(self, num_samples=1):
     #     """
