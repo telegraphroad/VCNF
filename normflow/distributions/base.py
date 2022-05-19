ba0fb261d8c62a5ba3415275406e491b82d3ab43
telegraphroad <saba.amiri@outlook.com>
2022-05-19 14:36:16 +0200

    shape=sample_shape

--- normflow/distributions/base.py
+++ normflow/distributions/base.py
@@ -95,7 +95,8 @@ class GenNormal(ExponentialFamily):
 
     def sample(self, sample_shape=torch.Size()):
         print('~~~~~~shape1',sample_shape)
-        shape = self._extended_shape(sample_shape)
+        #shape = self._extended_shape(sample_shape)
+        shape = sample_shape
         print('~~~~~~shape2',shape)
         #print('shape',shape)
         with torch.no_grad():
