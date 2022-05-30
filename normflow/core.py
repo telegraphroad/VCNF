58d915e5a2cf2fcfe911800e7fb156fdaa85dd6d
telegraphroad <saba.amiri@outlook.com>
2022-05-30 14:42:00 +0200

    redit

--- normflow/core.py
+++ normflow/core.py
@@ -117,6 +117,8 @@ class NormalizingFlow(nn.Module):
         :return: Samples, log probability
         """
         z, log_q = self.q0(num_samples)
+        print('88888',q0)
+        print('99999',z.shape,log_q.shape)
         for flow in self.flows:
             z, log_det = flow(z)
             log_q -= log_det
