2c98a77610eeaa2be25f819145f6a134e968088f
telegraphroad <saba.amiri@outlook.com>
2022-06-03 09:57:30 +0200

    mvn debug

--- normflow/core.py
+++ normflow/core.py
@@ -117,8 +117,8 @@ class NormalizingFlow(nn.Module):
         :return: Samples, log probability
         """
         z, log_q = self.q0(num_samples)
-        #print('88888',self.q0)
-        #print('99999',z.shape,log_q.shape)
+        print('88888',self.q0)
+        print('99999',z.shape,log_q.shape)
         for flow in self.flows:
             z, log_det = flow(z)
             log_q -= log_det
